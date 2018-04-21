2e58b2af28ddb742688b64d936d313e1a66c0069
Jaleesa McCrea <jaleesa@Jaleesas-MacBook-Pro.local>
2018-04-21 12:00:48 -0400

    one file, two servers

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 var http = require("http");
 
-var PORT = 7000;
+var PORT1 = 7000;
+var PORTTWO = 7500;
 
-function handleRequest(request, response) {
+
+function handleRequest1(request, response) {
     response.end("It Works!! Path Hit Baby!: " + request.url);
 }
 
-var server = http.createServer(handleRequest);
+function handleRequestTwo(request, response){
+    response.end("Path Hit! Woah Momma!! "+ request.url);
+}
 
-server.listen(PORT, function(){
-    console.log("Server listening on: http://localhost:"+ PORT);
+var server1 = http.createServer(handleRequest1);
+
+var serverTwo = http.createServer(handleRequestTwo);
+
+server1.listen(PORT1, function(){
+    console.log("Server listening on: http://localhost:"+ PORT1);
+});
+
+serverTwo.listen(PORTTWO, function(){
+    console.log("Server listening on: http://localhost:" + PORTTWO);
 });
